281a1682a0d4b51301914bee1e42bfdbdaf82ff6
Lily <lilyneedsanemail@gmail.com>
2021-11-22 22:29:09 -0500

    Getting started on matchmaking scene, made dateable character prefab

---
+++ Allergens Attract/Assets/Scripts/DateableCharacter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DateableCharacter : MonoBehaviour
+{
+    public string characterName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
